fa7bdf693376f41f44d6767442e594c23a5b4179
tinydeal <295174028@qq.com>
2013-03-05 09:41:05 +0800

    删除所有缓存，并将缓存移动到D盘根目录文件

--- config.php
+++ config.php
@@ -20,7 +20,7 @@ define("DIR_LANGUAGE", "$dir/catalog/language/");
 define("DIR_TEMPLATE", "$dir/catalog/view/theme/");
 define("DIR_CONFIG", "$dir/system/config/");
 define("DIR_IMAGE", "$dir/image/");
-define("DIR_CACHE", "$dir/system/cache/");
+define("DIR_CACHE", "D:\\cache\\");
 define("DIR_DOWNLOAD", "$dir/download/");
 define("DIR_LOGS", "$dir/system/logs/");
 
